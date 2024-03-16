f5046bb6d87ddeede8a28b05838b5669876423e5
PaperKurane <omginlol@gmail.com>
2024-03-16 18:37:58 +0800

    What even is this bug MouseEnterLeave

--- Y2_Event-Integ1-Collab_PrelimProj_WPF-8-Bit-Binary-Game/LeaderboardManager.cs
+++ Y2_Event-Integ1-Collab_PrelimProj_WPF-8-Bit-Binary-Game/LeaderboardManager.cs
@@ -9,37 +9,19 @@ namespace Y2_Event_Integ1_Collab_PrelimProj_WPF_8_Bit_Binary_Game
 {
     internal class LeaderboardManager
     {
-        public void EasyBoard(Dictionary<string, string[]> userData)
+        public void EasyBoard(List<KeyValuePair<string, string[]>> sortedUserData)
         {
-            if (userData.Count == 0)
-                return;
-
-            List<KeyValuePair<string, string[]>> sortedUserData = userData.ToList();
-
-            sortedUserData.Sort((x, y) =>
-            {
-                int xScore = int.Parse(x.Value[1]);
-                int yScore = int.Parse(y.Value[1]);
-                TimeSpan xTime = TimeSpan.Parse(x.Value[0]);
-                TimeSpan yTime = TimeSpan.Parse(y.Value[0]);
-
-                if (xScore != yScore)
-                    return yScore.CompareTo(xScore); // Sort by score in descending order
-                else
-                    return xTime.CompareTo(yTime); // If scores are equal, sort by time in ascending order
-            });
-
             LeaderBoardPlacer(sortedUserData);
         }
 
-        public void MediumBoard(Dictionary<string, string[]> userData)
+        public void MediumBoard(List<KeyValuePair<string, string[]>> sortedUserData)
         {
-
+            LeaderBoardPlacer(sortedUserData);
         }
 
-        public void HardBoard(Dictionary<string, string[]> userData)
+        public void HardBoard(List<KeyValuePair<string, string[]>> sortedUserData)
         {
-
+            LeaderBoardPlacer(sortedUserData);
         }
 
         private void LeaderBoardPlacer(List<KeyValuePair<string, string[]>> sortedUserData)
--- Y2_Event-Integ1-Collab_PrelimProj_WPF-8-Bit-Binary-Game/MainWindow.xaml.cs
+++ Y2_Event-Integ1-Collab_PrelimProj_WPF-8-Bit-Binary-Game/MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace Y2_Event_Integ1_Collab_PrelimProj_WPF_8_Bit_Binary_Game
     public partial class MainWindow : Window
     {
         private string _difficulty = null;
+        private Dictionary<string, string[]> _userData = new Dictionary<string, string[]>();
+        private List<KeyValuePair<string, string[]>> _sortedUserData = new List<KeyValuePair<string, string[]>>();
+
         private Dictionary<string, string[]> _userDataEasy = new Dictionary<string, string[]>();
         private Dictionary<string, string[]> _userDataMedium = new Dictionary<string, string[]>();
         private Dictionary<string, string[]> _userDataHard = new Dictionary<string, string[]>();
@@ -139,15 +142,15 @@ namespace Y2_Event_Integ1_Collab_PrelimProj_WPF_8_Bit_Binary_Game
         }
 
         #region Leaderboard Section
-        private void btnLeaderboard_MouseEnter(object sender, MouseEventArgs e)
-        {
-            lbLdrBrdName.Visibility = Visibility.Visible;
-        }
+        //private void btnLeaderboard_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    lbLdrBrdName.Visibility = Visibility.Visible;
+        //}
 
-        private void btnLeaderboard_MouseLeave(object sender, MouseEventArgs e)
-        {
-            lbLdrBrdName.Visibility = Visibility.Collapsed;
-        }
+        //private void btnLeaderboard_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    lbLdrBrdName.Visibility = Visibility.Collapsed;
+        //}
 
         private void btnLeaderboard_Click(object sender, RoutedEventArgs e)
         {
@@ -161,32 +164,69 @@ namespace Y2_Event_Integ1_Collab_PrelimProj_WPF_8_Bit_Binary_Game
 
         private void LeaderBoardHandler(string userName, string userTime, int userScore)
         {
+            string[] userData = { userTime, userScore.ToString() };
+
+            _userDataEasy["Claude Vincent Gallardo"] = new string[] { "00:01:21", "8" };
+            _userDataEasy["Orange Jungle Juice"] = new string[] { "00:00:59", "6" };
+            _userDataEasy["The Stingray on Desk"] = new string[] { "00:01:20", "8" };
+            _userDataEasy["Pipi"] = new string[] { "00:01:25", "8" };
+
             switch (_difficulty)
             {
                 case "Easy":
-                    _userDataEasy[userName] = new string[] {userTime, userScore.ToString()};
+                    _userDataEasy[userName] = userData;
+                    SortUserData(_userDataEasy);
                     break;
                 case "Medium":
-                    _userDataMedium[userName] = new string[] {userTime, userScore.ToString()};
+                    _userDataMedium[userName] = userData;
+                    SortUserData(_userDataMedium);
                     break;
                 case "Hard":
-                    _userDataMedium[userName] = new string[] {userTime, userScore.ToString()};
+                    _userDataHard[userName] = userData;
+                    SortUserData(_userDataHard);
                     break;
+            }
+        }
+
+        private void SortUserData(Dictionary<string, string[]> userData)
+        {
+            _sortedUserData = userData.ToList();
+
+            // Sirs Selection Sort Code thing
+            for (int x = 0; x < _sortedUserData.Count - 1; x++)
+            {
+                int minIndex = x;
+                for (int y = x + 1; y < _sortedUserData.Count; y++)
+                {
+                    int xScore = int.Parse(_sortedUserData[y].Value[1]);
+                    int yScore = int.Parse(_sortedUserData[minIndex].Value[1]);
+                    TimeSpan xTime = TimeSpan.Parse(_sortedUserData[y].Value[0]);
+                    TimeSpan yTime = TimeSpan.Parse(_sortedUserData[minIndex].Value[0]);
+
+                    if (xScore > yScore || (xScore == yScore && xTime < yTime))
+                        minIndex = y;
+                }
+
+                KeyValuePair<string, string[]> temp = _sortedUserData[x];
+                _sortedUserData[x] = _sortedUserData[minIndex];
+                _sortedUserData[minIndex] = temp;
             }
         }
 
         private void RadioButtonLeaderboard_Checked(object sender, RoutedEventArgs e)
         {
-            switch (_difficulty)
+            string difficulty = (sender as RadioButton)?.Name.Replace("rbLdrBrd", "");
+
+            switch (difficulty)
             {
                 case "Easy":
-                    _lm.EasyBoard(_userDataEasy);
+                    _lm.EasyBoard(_sortedUserData);
                     break;
                 case "Medium":
-                    _lm.MediumBoard(_userDataMedium);
+                    _lm.MediumBoard(_sortedUserData);
                     break;
                 case "Hard":
-                    _lm.HardBoard(_userDataHard);
+                    _lm.HardBoard(_sortedUserData);
                     break;
             }
         }
